384b26b8af2dcac89a6c15922454e08715c41627
Olia Medvetska <olia.medvetska1967@gmail.com>
2023-06-09 17:25:12 +0300

    add localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -47,6 +47,22 @@ class App extends Component {
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  
+// після кожного оновлення
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
